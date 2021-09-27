24067bc71ab2ff3ebce02b286afda325b8cbb2d4
LaTonya Johnson <latonya.johnson0921@gmail.com>
2021-09-27 16:34:51 -0500

    ddl

---
+++ cook.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE lets_cook;
+
+\c lets_cook;
+
+CREATE TABLE user
+(
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL
+);
+
+CREATE TABLE favorites
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES user,
+    meal_id INTEGER NOT NULL UNIQUE
+);
